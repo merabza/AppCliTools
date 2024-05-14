5e6f4aa2eeb7679f88d1f6678d360695f61d5073
merabza <merabza@gmail.com>
2024-05-15 00:22:00 +0400

    202405150021

--- CliParametersExcludeSetsEdit/MenuCommands/GenerateExcludeSetsCommand.cs
+++ CliParametersExcludeSetsEdit/MenuCommands/GenerateExcludeSetsCommand.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using CliMenu;
+﻿using CliMenu;
 using CliParametersExcludeSetsEdit.Generators;
 using LibDataInput;
 using LibFileParameters.Interfaces;
 using LibParameters;
-using SystemToolsShared;
 
 namespace CliParametersExcludeSetsEdit.MenuCommands;
 
@@ -12,6 +10,7 @@ public sealed class GenerateExcludeSetsCommand : CliMenuCommand
 {
     private readonly IParametersManager _parametersManager;
 
+    // ReSharper disable once ConvertToPrimaryConstructor
     public GenerateExcludeSetsCommand(IParametersManager parametersManager)
     {
         _parametersManager = parametersManager;
@@ -21,26 +20,14 @@ public sealed class GenerateExcludeSetsCommand : CliMenuCommand
     {
         MenuAction = EMenuAction.Reload;
         var parameters = (IParametersWithExcludeSets)_parametersManager.Parameters;
-        try
-        {
-            if (!Inputer.InputBool("This process will change Exclude Sets, are you sure?", false, false))
-                return;
 
-            StandardExcludeSetsGenerator standardExcludeSetsGenerator = new(_parametersManager);
-            standardExcludeSetsGenerator.Generate();
+        if (!Inputer.InputBool("This process will change Exclude Sets, are you sure?", false, false))
+            return;
 
-            //შენახვა
-            _parametersManager.Save(parameters, "ExcludeSets generated success");
-        }
-        catch (DataInputEscapeException)
-        {
-            Console.WriteLine();
-            Console.WriteLine("Escape... ");
-            StShared.Pause();
-        }
-        catch (Exception e)
-        {
-            StShared.WriteException(e, true);
-        }
+        StandardExcludeSetsGenerator standardExcludeSetsGenerator = new(_parametersManager);
+        standardExcludeSetsGenerator.Generate();
+
+        //შენახვა
+        _parametersManager.Save(parameters, "ExcludeSets generated success");
     }
 }
