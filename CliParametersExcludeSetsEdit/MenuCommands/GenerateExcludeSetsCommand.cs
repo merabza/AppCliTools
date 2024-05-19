44e7042ba7f593e08a4f9b2fa2320384e582f80e
merabza <merabza@gmail.com>
2024-05-19 16:48:42 +0400

    202405191648

--- CliParametersExcludeSetsEdit/MenuCommands/GenerateExcludeSetsCommand.cs
+++ CliParametersExcludeSetsEdit/MenuCommands/GenerateExcludeSetsCommand.cs
@@ -11,23 +11,24 @@ public sealed class GenerateExcludeSetsCommand : CliMenuCommand
     private readonly IParametersManager _parametersManager;
 
     // ReSharper disable once ConvertToPrimaryConstructor
-    public GenerateExcludeSetsCommand(IParametersManager parametersManager)
+    public GenerateExcludeSetsCommand(IParametersManager parametersManager) : base(null, EMenuAction.Reload)
     {
         _parametersManager = parametersManager;
     }
 
-    protected override void RunAction()
+    protected override bool RunBody()
     {
-        MenuAction = EMenuAction.Reload;
+
         var parameters = (IParametersWithExcludeSets)_parametersManager.Parameters;
 
         if (!Inputer.InputBool("This process will change Exclude Sets, are you sure?", false, false))
-            return;
+            return false;
 
         StandardExcludeSetsGenerator standardExcludeSetsGenerator = new(_parametersManager);
         standardExcludeSetsGenerator.Generate();
 
         //შენახვა
         _parametersManager.Save(parameters, "ExcludeSets generated success");
+        return true;
     }
 }
