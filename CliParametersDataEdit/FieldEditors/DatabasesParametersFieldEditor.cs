9f0542a2ab3e22a43155764f7f43c7db628460df
merabza <merabza@gmail.com>
2025-01-21 07:34:53 +0400

    202501210734

---
+++ CliParametersDataEdit/FieldEditors/DatabasesParametersFieldEditor.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http;
+using CliParameters;
+using CliParameters.FieldEditors;
+using CliParametersDataEdit.ParametersEditors;
+using LibDatabaseParameters;
+using LibParameters;
+using Microsoft.Extensions.Logging;
+
+namespace CliParametersDataEdit.FieldEditors;
+
+public sealed class DatabasesParametersFieldEditor : ParametersFieldEditor<DatabasesParameters,
+    DatabaseParametersEditor>
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    // ReSharper disable once ConvertToPrimaryConstructor
+    public DatabasesParametersFieldEditor(ILogger logger, IHttpClientFactory httpClientFactory, string propertyName,
+        IParametersManager parametersManager) : base(logger, propertyName, parametersManager)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    protected override DatabaseParametersEditor CreateEditor(object record, DatabasesParameters currentValue)
+    {
+        var serverDatabasesExchangeParametersManager =
+            new SubParametersManager<DatabasesParameters>(currentValue, ParametersManager, this, record);
+
+        return new DatabaseParametersEditor(Logger, _httpClientFactory, serverDatabasesExchangeParametersManager,
+            ParametersManager);
+    }
+}
