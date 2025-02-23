ccc2888e89f91c259754d0c103416985de14f6c0
merabza <merabza@gmail.com>
2025-02-23 18:12:33 +0400

    202502231812

--- DbContextAnalyzer/CodeCreators/ProjectDataSeedersFabricCreator.cs
+++ DbContextAnalyzer/CodeCreators/ProjectDataSeedersFabricCreator.cs
@@ -33,7 +33,7 @@ public sealed class ProjectDataSeedersFabricCreator : CodeCreator
             "using Microsoft.AspNetCore.Identity",
             _isAnyCarcassType
                 ? $"using {_parameters.ProjectNamespace}.{_parameters.CarcassSeedersFolderName}"
-                : string.Empty, 
+                : string.Empty,
             //$"using {_parameters.ProjectNamespace}.{_parameters.ProjectSeedersFolderName}",
             $"namespace {_parameters.ProjectNamespace}", string.Empty, new CodeBlock(
                 $"public /*open*/ class {_parameters.ProjectDataSeedersFabricClassName} : DataSeedersFabric",
