7a1870f7ceb217e918be0ac257c96a77ce0d7a75
merabza <merabza@gmail.com>
2026-01-28 18:59:36 +0400

    202601281859

--- AppCliTools.DbContextAnalyzer/CodeCreators/ServiceCreatorCreator.cs
+++ AppCliTools.DbContextAnalyzer/CodeCreators/ServiceCreatorCreator.cs
@@ -26,21 +26,21 @@ public sealed class ServiceCreatorCreator : CodeCreator
         string projectDbContextClassName = _par.ProjectDbContextClassName; // + "DbContext"; //GeoModelDbContext
 
         var block = new CodeBlock(string.Empty, new OneLineComment($"Created by {GetType().Name} at {DateTime.Now}"),
-            "using BackendCarcass.Database", "using BackendCarcass.DataSeeding", "using BackendCarcass.Db",
-            "using CarcassIdentity", "using BackendCarcass.MasterData.Models", string.Empty,
-            $"using {dataSeedingProjectName}", $"using {projectDbProjectName}", $"using {newDataSeedingProjectName}",
-            string.Empty, "using Microsoft.AspNetCore.Identity", "using Microsoft.EntityFrameworkCore",
+            "using BackendCarcass.Database", "using BackendCarcass.DataSeeding", "using BackendCarcass.Identity",
+            "using BackendCarcass.MasterData.Models", string.Empty, $"using {dataSeedingProjectName}",
+            $"using {projectDbProjectName}", $"using {newDataSeedingProjectName}", string.Empty,
+            "using Microsoft.AspNetCore.Identity", "using Microsoft.EntityFrameworkCore",
             "using Microsoft.Extensions.DependencyInjection", "using SystemTools.SystemToolsShared", string.Empty,
             $"namespace {_par.SeedProjectNamespace}", string.Empty, new CodeBlock(
                 "public sealed class SeedDbServicesCreator : ServicesCreator", string.Empty,
                 "private readonly string _connectionString", string.Empty,
-                "private readonly string _excludesRulesParametersFilePath", string.Empty,
-                new OneLineComment(" ReSharper disable once ConvertToPrimaryConstructor"),
-                new CodeBlock(
-                    "public SeedDbServicesCreator(string? logFolder, string? logFileName, string appName, string connectionString, string excludesRulesParametersFilePath) : base(logFolder, logFileName, appName)",
-                    "_connectionString = connectionString",
-                    "_excludesRulesParametersFilePath = excludesRulesParametersFilePath"), string.Empty, new CodeBlock(
-                    "protected override void ConfigureServices(IServiceCollection services)",
+                //"private readonly string _excludesRulesParametersFilePath", string.Empty,
+                new OneLineComment(" ReSharper disable once ConvertToPrimaryConstructor"), new CodeBlock(
+                    "public SeedDbServicesCreator(string? logFolder, string? logFileName, string appName, string connectionString) : base(logFolder, logFileName, appName)", //, string excludesRulesParametersFilePath
+                    "_connectionString = connectionString"
+                    //,
+                    //"_excludesRulesParametersFilePath = excludesRulesParametersFilePath"
+                ), string.Empty, new CodeBlock("protected override void ConfigureServices(IServiceCollection services)",
                     "base.ConfigureServices(services)", new OneLineComment("identity"),
                     "services.AddScoped<IUserStore<AppUser>, MyUserStore>()",
                     "services.AddScoped<IUserPasswordStore<AppUser>, MyUserStore>()",
